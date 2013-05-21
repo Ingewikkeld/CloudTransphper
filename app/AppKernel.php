56aaf7e45e486eb941f9de4af42090245c661e80
Stefan Koopmanschap <left@leftontheweb.com>
2013-05-21 13:00:37 +0200

    send logs and cache to a writable directory

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,4 +35,14 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        return '/tmp/cache';
+    }
+
+    public function getLogDir()
+    {
+        return '/tmp/logs';
+    }
 }

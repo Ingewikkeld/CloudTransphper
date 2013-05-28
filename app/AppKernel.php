483a2d71d8238ff286751568f00e8353081e1039
Stefan Koopmanschap <left@leftontheweb.com>
2013-05-28 14:36:16 +0200

    enabled IngewikkeldHPCloudBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Ingewikkeld\CloudTransphperBundle\IngewikkeldCloudTransphperBundle(),
+            new Ingewikkeld\HPCloudBundle\IngewikkeldHPCloudBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
